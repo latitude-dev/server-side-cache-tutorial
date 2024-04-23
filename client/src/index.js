096e582364746cd1aae90a2c5794e1f5b36bced6
Carlos Sansón <carlos@latitude.so>
2024-04-23 14:43:17 +0200

    Added tutorial code

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Theme>
+    <App />
+  </Theme>
+);
